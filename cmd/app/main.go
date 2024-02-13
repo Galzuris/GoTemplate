c20a802dfbd71d5ed4c5ece45bccf7ce88bad984
Kirill <galzuris@gmail.com>
2024-02-13 07:01:22 +0000

    init

---
+++ cmd/app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.RemoteAddr)
+	log.Println("hello request", r.RemoteAddr)
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	log.Println("server started")
+	http.ListenAndServe(":4500", nil)
+}
